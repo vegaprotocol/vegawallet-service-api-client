8950835746e15b45d207a4faa29690dbfb5a8787
madalinaraicu <madalina@vegaprotocol.io>
2022-06-29 09:48:19 +0100

    fix: #470 .gitignore node_modules

--- http/http.ts
+++ http/http.ts
@@ -39,81 +39,81 @@ export type RequestBody = undefined | string | FormData | URLSearchParams;
  * Represents an HTTP request context
  */
 export class RequestContext {
-    private headers: { [key: string]: string } = {};
-    private body: RequestBody = undefined;
-    private url: URLParse;
+  private headers: { [key: string]: string } = {};
+  private body: RequestBody = undefined;
+  private url: any;
 
-    /**
-     * Creates the request context using a http method and request resource url
-     *
-     * @param url url of the requested resource
-     * @param httpMethod http method
-     */
-    public constructor(url: string, private httpMethod: HttpMethod) {
-        this.url = new URLParse(url, true);
-    }
+  /**
+   * Creates the request context using a http method and request resource url
+   *
+   * @param url url of the requested resource
+   * @param httpMethod http method
+   */
+  public constructor(url: string, private httpMethod: HttpMethod) {
+    this.url = new URLParse(url, true);
+  }
 
-    /*
-     * Returns the url set in the constructor including the query string
-     *
-     */
-    public getUrl(): string {
-        return this.url.toString();
-    }
+  /*
+   * Returns the url set in the constructor including the query string
+   *
+   */
+  public getUrl(): string {
+    return this.url.toString();
+  }
 
-    /**
-     * Replaces the url set in the constructor with this url.
-     *
-     */
-    public setUrl(url: string) {
-        this.url = new URLParse(url, true);
-    }
+  /**
+   * Replaces the url set in the constructor with this url.
+   *
+   */
+  public setUrl(url: string) {
+    this.url = new URLParse(url, true);
+  }
 
-    /**
-     * Sets the body of the http request either as a string or FormData
-     *
-     * Note that setting a body on a HTTP GET, HEAD, DELETE, CONNECT or TRACE
-     * request is discouraged.
-     * https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#rfc.section.7.3.1
-     *
-     * @param body the body of the request
-     */
-    public setBody(body: RequestBody) {
-        this.body = body;
-    }
+  /**
+   * Sets the body of the http request either as a string or FormData
+   *
+   * Note that setting a body on a HTTP GET, HEAD, DELETE, CONNECT or TRACE
+   * request is discouraged.
+   * https://httpwg.org/http-core/draft-ietf-httpbis-semantics-latest.html#rfc.section.7.3.1
+   *
+   * @param body the body of the request
+   */
+  public setBody(body: RequestBody) {
+    this.body = body;
+  }
 
-    public getHttpMethod(): HttpMethod {
-        return this.httpMethod;
-    }
+  public getHttpMethod(): HttpMethod {
+    return this.httpMethod;
+  }
 
-    public getHeaders(): { [key: string]: string } {
-        return this.headers;
-    }
+  public getHeaders(): { [key: string]: string } {
+    return this.headers;
+  }
 
-    public getBody(): RequestBody {
-        return this.body;
-    }
+  public getBody(): RequestBody {
+    return this.body;
+  }
 
-    public setQueryParam(name: string, value: string) {
-        let queryObj = this.url.query;
-        queryObj[name] = value;
-        this.url.set("query", queryObj);
-    }
+  public setQueryParam(name: string, value: string) {
+    let queryObj = this.url.query;
+    queryObj[name] = value;
+    this.url.set("query", queryObj);
+  }
 
-    /**
-     * Sets a cookie with the name and value. NO check  for duplicate cookies is performed
-     *
-     */
-    public addCookie(name: string, value: string): void {
-        if (!this.headers["Cookie"]) {
-            this.headers["Cookie"] = "";
-        }
-        this.headers["Cookie"] += name + "=" + value + "; ";
+  /**
+   * Sets a cookie with the name and value. NO check  for duplicate cookies is performed
+   *
+   */
+  public addCookie(name: string, value: string): void {
+    if (!this.headers["Cookie"]) {
+      this.headers["Cookie"] = "";
     }
+    this.headers["Cookie"] += name + "=" + value + "; ";
+  }
 
-    public setHeaderParam(key: string, value: string): void  {
-        this.headers[key] = value;
-    }
+  public setHeaderParam(key: string, value: string): void {
+    this.headers[key] = value;
+  }
 }
 
 export interface ResponseBody {
